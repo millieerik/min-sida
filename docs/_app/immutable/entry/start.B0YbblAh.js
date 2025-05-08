62a1436f6494204a1f4358e0cde561ee458a20fe
Millie Eriksson <mileri@elevad.umea.se>
2025-05-08 13:02:24 +0200

    fix

---
+++ docs/_app/immutable/entry/start.B0YbblAh.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/YIjtX6I9.js";export{o as load_css,r as start};
--- docs/_app/immutable/entry/start.EH5BQjln.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/Q_gK6SJv.js";export{o as load_css,r as start};
